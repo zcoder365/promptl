acc027bc1ed32981c361d54e97bff25d1d0afcae
Zoe <zoedroulias@gmail.com>
2024-10-14 21:07:32 -0400

    create directory for data in the data.py file; connect to the database

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+USER_DATA = "../data/users.db"
+WRITING_DATA = "../data/stories.db"
+
+# connect to a (new) database
+conn = sqlite3.connect(USER_DATA)
+
+# close connection
+conn.close()

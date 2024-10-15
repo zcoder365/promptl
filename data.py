f1e1772358109243b645f57963f5eb0673d564a0
Zoe <zoedroulias@gmail.com>
2024-10-14 21:08:49 -0400

    add a function for accessing the user database specifically

--- data.py
+++ data.py
@@ -3,8 +3,9 @@ import sqlite3
 USER_DATA = "../data/users.db"
 WRITING_DATA = "../data/stories.db"
 
-# connect to a (new) database
-conn = sqlite3.connect(USER_DATA)
-
-# close connection
-conn.close()
+def get_user_data():
+    # connect to the database
+    conn = sqlite3.connect(USER_DATA)
+    
+    # close the connection
+    conn.close()

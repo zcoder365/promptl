231e9ce330c7d758b90ebaa3947a1139ed903e00
Zoe <zoedroulias@gmail.com>
2024-10-14 21:22:23 -0400

    refactor get_data to add_data and change functionality

--- data/data.py
+++ data/data.py
@@ -3,9 +3,9 @@ import sqlite3
 USER_DATA = "data/users.db"
 STORY_DATA = "data/stories.db"
 
-def get_data(file_path: str):
+def add_data(data: list):
     # connect to the database
-    conn = sqlite3.connect(file_path)
+    conn = sqlite3.connect(USER_DATA)
     
     # create a cursor
     cur = conn.cursor()
@@ -15,15 +15,15 @@ def get_data(file_path: str):
                 (username TEXT, password TEXT)''')
     conn.commit()
     
-    test_data = [
-        ("zkd365", "test"),
-        ("sushi", "test")
-    ]
-    
     cur.executemany("""INSERT INTO users (username, password) VALUES (?, ?)""", test_data)
     
     # close the database objects
     cur.close()
     conn.close()
     
-get_data(STORY_DATA)
+test_data = [
+    ("zkd365", "test"),
+    ("sushi", "test")
+]
+
+add_data(test_data)

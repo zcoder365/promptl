434dc1aae4505d30b2959593f9fd5f05a725923f
Zoe <zoedroulias@gmail.com>
2024-10-15 16:23:16 -0400

    create model and add a function for getting story length

---
+++ model.py
@@ -0,0 +1,47 @@
+def get_story_length(story, prompts):
+    points = 0
+    
+    # create a list for the story
+    story = story.split(' ')
+    
+    # get it's length/number of words
+    words_written = len(story)
+    
+    if words_written >= 70:
+        if prompts['name'] in story.upper():
+            points += 10
+            words_used += 1
+            
+        elif prompts['name'] not in story.upper():
+            pass
+        
+        if prompts['job'] in story.upper():
+            points += 10
+            
+            words_used += 1
+
+        elif prompts['job'] not in story.upper():
+            pass
+        
+        if prompts['object'] in story.upper():
+            points += 10
+            words_used += 1
+            
+        elif prompts['object'] not in story.upper():
+            pass
+        
+        if prompts['place'] in story.upper():
+            points += 10
+            words_used += 1
+            
+        elif prompts['place'] not in story.upper():
+            pass
+        
+        if prompts['bonus'] in story.upper():
+            points += 20
+            words_used += 1
+            
+        elif prompts['bonus'] not in story.upper():
+            pass
+        
+    return {"story_length": words_written, "points": points}

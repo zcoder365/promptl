de5a4e9a97dd59f52a54540feae0135658417850
Zoe <zoedroulias@gmail.com>
2024-10-14 21:25:22 -0400

    create a function for viewing a database with sqlite3 in data.py

--- data/data.py
+++ data/data.py
@@ -20,7 +20,20 @@ def add_data(data: list):
     # close the database objects
     cur.close()
     conn.close()
+
+def view_data(database_directory: str, table_name: str):
+    # connect to the database
+    conn = sqlite3.connect(database_directory)
     
+    # create a cursor
+    cur = conn.cursor()
+    
+    for row in cur.execute("SELECT * FROM " + table_name+ ";"):
+        print(row)
+        
+    cur.close()
+    conn.close()
+
 test_data = [
     ("zkd365", "test"),
     ("sushi", "test")

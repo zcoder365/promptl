0708783f0350ade20dd86295c2c66b589e8fdedb
Zoe <zoedroulias@gmail.com>
2024-10-15 10:20:20 -0400

    update user_exists function; add imports

--- accounts.py
+++ accounts.py
@@ -1,4 +1,23 @@
+import session, request
 import data.data
 
 def user_exists(username, password, parent_email):
-    pass
+    # determine if the user exists
+    existing_user = data.data.find_user(username)
+    
+    if not existing_user: # if user doesn't exist...
+        # add the user with generic info
+        # users.insert_one({ 'username': username, 'password': str(hashpass, 'utf-8'), 'parent_email': parent_email, 'points': 0, 'streak': 0, "prizes": 0, "average_words": 0})
+        
+        # generate the user data
+        user_data = [(username, password, parent_email, 0, 0)]
+        
+        # add the data using the function in the data.py file in the data folder
+        data.data.add_user_data(user_data)
+        
+        # create a session
+        session['username'] = request.form['username']
+    
+    elif existing_user: # if the user exists...
+        # return the error message
+        return 'That username already exists! Try logging in.'
--- main.py
+++ main.py
@@ -58,25 +58,7 @@ def signup_check():
     password = request.form.get("password")
     parent_email = request.form.get("parent-email")
     
-    # determine if the user exists
-    existing_user = data.data.find_user(username)
     
-    if not existing_user: # if user doesn't exist...
-        # add the user with generic info
-        # users.insert_one({ 'username': username, 'password': str(hashpass, 'utf-8'), 'parent_email': parent_email, 'points': 0, 'streak': 0, "prizes": 0, "average_words": 0})
-        
-        # generate the user data
-        user_data = [(username, password, parent_email, 0, 0)]
-        
-        # add the data using the function in the data.py file in the data folder
-        data.data.add_user_data(user_data)
-        
-        # create a session
-        session['username'] = request.form['username']
-    
-    elif existing_user: # if the user exists...
-        # return the error message
-        return 'That username already exists! Try logging in.'
         
     # redirect the user to the home/writing page
     return redirect(url_for("home"))

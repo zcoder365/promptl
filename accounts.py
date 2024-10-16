4d35f77d4dccd5eaa12189afa378e85e1cf45fa1
Zoe <zoedroulias@gmail.com>
2024-10-16 10:22:14 -0400

    remove indents from prior line

--- accounts.py
+++ accounts.py
@@ -10,7 +10,7 @@ def add_user(username: str, password: str, email: str):
     if is_user:
         return "User already exists. Can't create a new account with this username."
     
-    elif not is_user:        
+    elif not is_user:
         # generate the user data
         user_data = [(username, password, email, 0, 0)]
         

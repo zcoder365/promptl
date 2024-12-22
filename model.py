8d006a0021dde4a540f4181759298d2b2b4743e4
Zoe <zoedroulias@gmail.com>
2024-12-22 14:14:37 -0500

    update story.upper() to story_text

--- model.py
+++ model.py
@@ -7,40 +7,40 @@ def get_story_length_and_points(story, prompts):
     story_text = " ".join(story).upper() # need to fix the story checking since it's a list, not a string
 
     if words_written >= 70:
-        if prompts['name'] in story.upper():
+        if prompts['name'] in story_text:
             points += 10
             words_used += 1
             
-        elif prompts['name'] not in story.upper():
+        elif prompts['name'] not in story_text:
             pass
         
-        if prompts['job'] in story.upper():
+        if prompts['job'] in story_text:
             points += 10
             
             words_used += 1
 
-        elif prompts['job'] not in story.upper():
+        elif prompts['job'] not in story_text:
             pass
         
-        if prompts['object'] in story.upper():
+        if prompts['object'] in story_text:
             points += 10
             words_used += 1
             
-        elif prompts['object'] not in story.upper():
+        elif prompts['object'] not in story_text:
             pass
         
-        if prompts['place'] in story.upper():
+        if prompts['place'] in story_text:
             points += 10
             words_used += 1
             
-        elif prompts['place'] not in story.upper():
+        elif prompts['place'] not in story_text:
             pass
         
-        if prompts['bonus'] in story.upper():
+        if prompts['bonus'] in story_text:
             points += 20
             words_used += 1
             
-        elif prompts['bonus'] not in story.upper():
+        elif prompts['bonus'] not in story_text:
             pass
         
     return {"story_length": words_written, "points": points}

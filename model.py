f77d5a88dfb5e8d3d3a31d85d66ab3522212c85e
Zoe <zoedroulias@gmail.com>
2024-12-22 14:15:25 -0500

    remove elif conditions since we only care about the conditions if the word is in the story

--- model.py
+++ model.py
@@ -10,37 +10,21 @@ def get_story_length_and_points(story, prompts):
         if prompts['name'] in story_text:
             points += 10
             words_used += 1
-            
-        elif prompts['name'] not in story_text:
-            pass
         
         if prompts['job'] in story_text:
             points += 10
-            
             words_used += 1
-
-        elif prompts['job'] not in story_text:
-            pass
         
         if prompts['object'] in story_text:
             points += 10
             words_used += 1
-            
-        elif prompts['object'] not in story_text:
-            pass
         
         if prompts['place'] in story_text:
             points += 10
             words_used += 1
-            
-        elif prompts['place'] not in story_text:
-            pass
         
         if prompts['bonus'] in story_text:
             points += 20
             words_used += 1
-            
-        elif prompts['bonus'] not in story_text:
-            pass
         
     return {"story_length": words_written, "points": points}

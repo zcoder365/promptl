65e4dfe5f6c9f789b09c6cf1c00460520c1b6b96
Zoe <zoedroulias@gmail.com>
2024-12-23 09:58:37 -0500

    create new process and remove old process for checking points

--- model.py
+++ model.py
@@ -3,25 +3,22 @@ def get_story_length_and_points(story, prompts):
     words_written = len(story) # get story length
     story_text = " ".join(story).upper() # convert story to a list
 
+    # only calculate points if minimum word count is met
     if words_written >= 70:
-        if prompts['name'] in story_text:
-            points += 10
-            words_used += 1
+        # points for each prompt word
+        prompt_points = {
+            'name': 10,
+            'job': 10,
+            'object': 10,
+            'place': 10,
+            'bonus': 20
+        }
         
-        if prompts['job'] in story_text:
-            points += 10
-            words_used += 1
-        
-        if prompts['object'] in story_text:
-            points += 10
-            words_used += 1
-        
-        if prompts['place'] in story_text:
-            points += 10
-            words_used += 1
-        
-        if prompts['bonus'] in story_text:
-            points += 20
-            words_used += 1
+        # check each prompt word
+        for prompt_type, word in prompts.items():
+            # if the word is in the story...
+            if word.upper() in story_text:
+                # add the points
+                points += prompt_points[prompt_type]
         
     return {"story_length": words_written, "points": points}

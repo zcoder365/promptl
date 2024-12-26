818edbc9804b727cf3c8a52ac891cc9a9f1f8b0e
Zoe <zoedroulias@gmail.com>
2024-12-26 17:09:14 -0500

    update functions called from pymongo

--- helpers/data_management.py
+++ helpers/data_management.py
@@ -14,7 +14,7 @@ except Exception as e:
     print(e)
 
 def save_document_to_db(uri, db_name, collection_name, document):
-    client = pymongo.MongoClient(uri)
+    client = MongoClient(uri)
     db = client[db_name]
     collection = db[collection_name]
     result = collection.insert_one(document)

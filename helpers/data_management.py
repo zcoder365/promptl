00229bf70941c516528eea5b96ab9701f0d61a4c
Zoe <zoedroulias@gmail.com>
2024-12-26 17:34:59 -0500

    comment out pings

--- helpers/data_management.py
+++ helpers/data_management.py
@@ -10,12 +10,12 @@ uri = os.getenv("MONGODB_URI")
 # create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"))
 
-# send a ping to confirm a successful connection
-try:
-    client.admin.command("ping")
-    print("Successfully connected to the server.")
-except Exception as e:
-    print(e)
+# # send a ping to confirm a successful connection
+# try:
+#     client.admin.command("ping")
+#     print("Successfully connected to the server.")
+# except Exception as e:
+#     print(e)
 
 def save_document_to_db(uri, db_name, collection_name, document):
     client = MongoClient(uri)

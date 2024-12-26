df4fc2930ac55f3cad6c9cdbaae102ea0cf58e66
Zoe <zoedroulias@gmail.com>
2024-12-26 17:07:44 -0500

    connect to server; have a var for the uri

--- helpers/data_management.py
+++ helpers/data_management.py
@@ -1,6 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+uri = "" # create env var for this
+
+# create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi("1"))
+
 def save_document_to_db(uri, db_name, collection_name, document):
     client = pymongo.MongoClient(uri)
     db = client[db_name]

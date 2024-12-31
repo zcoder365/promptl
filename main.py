9296d03a7696e3c48d641b40b73b30a71a5dcfd4
Zoe <zoedroulias@gmail.com>
2024-12-31 17:47:34 -0500

    create databases

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_URI') # get the URI for the database
 db = SQLAlchemy(app) # set up the SQLAlchemy database
 
+# create user database
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+# create stories database
+class Story(db.Model):
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    story_content = db.Column(db.String(800), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+
+# create all of the databases
+with app.app_context():
+    db.create_all()
+
 # global variables
 prps = None
 

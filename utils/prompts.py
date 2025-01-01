c3d6e5aeb11da079e550f4d7513b2cea67738d6c
Zoe <zoedroulias@gmail.com>
2025-01-01 16:23:04 -0500

    add comments to prompts.py

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,13 +1,16 @@
 import random
 
+# set the file names for the prompts
 BONUS_FILE = "bonus.txt"
 JOBS_FILE = "jobs.txt"
 NAMES_FILE = "names.txt"
 OBJECTS_FILE = "objects.txt"
 PLACES_FILE = "places.txt"
 
+# set the prompt file names as an array
 prompt_file_names = [BONUS_FILE, JOBS_FILE, NAMES_FILE, OBJECTS_FILE, PLACES_FILE]
 
+# generate a single prompt using a specific file
 def gen_prompt(file_name):
     with open("text/" + file_name, "r") as f:
         data = f.read()
@@ -16,9 +19,11 @@ def gen_prompt(file_name):
     prompt = random.choice(data2)
     return prompt.upper()
 
+# generate all the prompts using all the files
 def gen_all_prompts():
     prompts = {}
     
+    # add keys to the prompts dict to be able to generate the variables
     for file in prompt_file_names:
         if "bonus" in file:
             key = "bonus"
@@ -31,10 +36,13 @@ def gen_all_prompts():
         elif "places" in file:
             key = "place"
         
+        # generate the prompts using the gen_prompt()  function
         prompts[key] = gen_prompt(file)
-        
+    
+    # return the prompts
     return prompts
 
+# generate a compliment for the results page
 def gen_compliment():
 	compliments = ['Great Job!', 'Excellent Work!', 'Super Job!', 'Way to Go!']
 

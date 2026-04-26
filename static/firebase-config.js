e2a945a324474108956c150aaada54192480a0c9
Zoe <zoedroulias@gmail.com>
2026-04-26 08:49:44 -0400

    create firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,9 @@
+// shared firebase config — imported by login, signup, and template
+export const firebaseConfig = {
+    apiKey: "AIzaSyBjyTaTEsvSqsAUo6dyMhtZpjG2gNTse2c",
+    authDomain: "promptl-35e17.firebaseapp.com",
+    projectId: "promptl-35e17",
+    storageBucket: "promptl-35e17.firebasestorage.app",
+    messagingSenderId: "434478496754",
+    appId: "1:434478496754:web:94b392bc6f210d53ff096b"
+};
